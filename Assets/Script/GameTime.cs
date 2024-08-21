3a637fbdbb7b32e104c433751d1d8676d1d06223
sekineseishu <mika242401@gmail.com>
2024-08-21 21:43:48 +0900

    ゲーム時間の管理をする

--- Assets/Script/GameTime.cs
+++ Assets/Script/GameTime.cs
@@ -5,19 +5,19 @@ using TMPro;
 
 public class GameTime : MonoBehaviour
 {
-    [SerializeField] private TextMeshProUGUI timeText; // 時間を表示するUIテキスト
-    [SerializeField] private GameObject GameClear;
-    [SerializeField] private GameObject result;
-    private bool Clear;
-    public AudioSource playeraudiosource;
+    [SerializeField] private TMP_Text _timeText; // 時間を表示するUIテキスト
+    [SerializeField] private GameObject _gameClear;//ゲームクリアオブジェクト
+    [SerializeField] private GameObject _result;//リザルト
+    [SerializeField] private AudioSource _playerAudioSource;
+    private bool _clear;//クリアフラグ
 
-    public float elapsedTime = 0f; // 経過時間（秒単位）
+    private float _elapsedTime = 0f; // 経過時間（秒単位）
 
     // Start is called before the first frame update
     void Start()
     {
         //ゲームクリアを可能にする
-        Clear = true;
+        _clear = true;
         Time.timeScale = 1;
     }
 
@@ -25,24 +25,24 @@ public class GameTime : MonoBehaviour
     void Update()
     {
         // 経過時間を更新
-        elapsedTime += Time.deltaTime;
+        _elapsedTime += Time.deltaTime;
 
         // 経過時間を分と秒に変換
-        int minutes = Mathf.FloorToInt(elapsedTime / 60);
-        int seconds = Mathf.FloorToInt(elapsedTime % 60);
+        int minutes = Mathf.FloorToInt(_elapsedTime / 60);
+        int seconds = Mathf.FloorToInt(_elapsedTime % 60);
 
         // 時間をテキストに表示
-        timeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+        _timeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
 
 
-        if (elapsedTime >= 180 && Clear == true)
+        if (_elapsedTime >= 180 && _clear == true)
         {
             //時間が達してクリア可能状態なら時間を止めてテキストとボタンを表示
-            playeraudiosource.Stop();
-            GameClear.SetActive(true);
-            result.SetActive(true);
+            _playerAudioSource.Stop();
+            _gameClear.SetActive(true);
+            _result.SetActive(true);
             Time.timeScale = 0;
-            Clear = false;
+            _clear = false;
         }
     }
 }
