ff440b23bf6e21b7fd40ea608275d83f2e50444f
sekineseishu <mika242401@gmail.com>
2024-08-21 21:46:00 +0900

    魔法陣スキルの詳細

--- Assets/Script/Magic.cs
+++ Assets/Script/Magic.cs
@@ -4,20 +4,20 @@ using UnityEngine;
 
 public class Magic : MonoBehaviour
 {
-    [SerializeField] private Skill _skill;
-    public float growthRate; // 成長速度（秒ごとのスケールの増加率）
-    public float SkillDamage;
-    public float playtime;
+    [SerializeField] private Skill _skill;//スキルデータ
+    [SerializeField] private float growthRate; // 成長速度（秒ごとのスケールの増加率）
+    [SerializeField] private float _skillDamage;//スキルダメージ
+    [SerializeField] private float playtime;//攻撃継続時間
     public Player _player;
 
-    public AudioSource _audiosource;
-    public AudioClip MagicSE;
+    [SerializeField] private AudioSource _audiosource;
+    [SerializeField] private AudioClip _magicSE;
     // Start is called before the first frame update
     void Start()
     {
+        //スキルデータを渡して初期化してSEを流す
         playtime = _skill._playTime;
-        _audiosource = GetComponent<AudioSource>();
-        _audiosource.PlayOneShot(MagicSE);
+        _audiosource.PlayOneShot(_magicSE);
     }
 
     // Update is called once per frame
@@ -34,8 +34,7 @@ public class Magic : MonoBehaviour
         // オブジェクトのスケールを変更
         transform.localScale = currentScale;
 
-        //transform.position = new Vector3(_player.position.x, _player.position.y, _player.position.z);
-
+        //一定時間経ったら消える
         if (playtime <= 0)
         {
             Destroy(gameObject);
@@ -50,7 +49,7 @@ public class Magic : MonoBehaviour
         
         if (collision.gameObject.tag == "Enemy")
         {
-            //プレイヤーの攻撃力とスキルのダメージ
+            //プレイヤーの攻撃力とスキルのダメージで計算して敵にダメージを与える
             enemy.HitDamage(_player._attack * _skill._damage);
         }
     }
