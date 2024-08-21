e619edcc9cb1d8163a4d3f2713078eead488595f
sekineseishu <mika242401@gmail.com>
2024-08-21 19:16:25 +0900

    スカルスキルの詳細

--- Assets/Script/Skull.cs
+++ Assets/Script/Skull.cs
@@ -4,19 +4,20 @@ using UnityEngine;
 
 public class Skull : MonoBehaviour
 {
-    [SerializeField] private Skill _skill;
-    public Player _centerPoint; // プレイヤーの周りを回る中心点
+    [SerializeField] private Skill _skill;//スキルデータ
+    public Player _player; // プレイヤーの周りを回る中心点
     public float _radius = 2.0f; // 半径
     private float _speed = 2.0f; // 回転速度
     public int _skillDamage;
 
-    public float _angle;
+    public float _angle;//角度
 
-    private float _playTime;
+    private float _playTime;//攻撃生存時間
 
     // Start is called before the first frame update
     void Start()
     {
+        //ダメージと攻撃生存時間をスキルデータから受け取る
         _skillDamage = _skill._damage;
         _playTime = _skill._playTime;
     }
@@ -24,7 +25,7 @@ public class Skull : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        //一定時間が経ったら消える
         _playTime -= Time.deltaTime;
         if (_playTime <= 0)
         {
@@ -36,7 +37,7 @@ public class Skull : MonoBehaviour
 
         // 新しい位置を計算
         Vector3 offset = new Vector3(Mathf.Cos(_angle), Mathf.Sin(_angle), 0) * _radius;
-        transform.position = _centerPoint.transform.position + offset;
+        transform.position = _player.transform.position + offset;
 
 
     }
@@ -46,9 +47,8 @@ public class Skull : MonoBehaviour
         Enemy enemy = collision.GetComponent<Enemy>();
         if (collision.gameObject.tag == "Enemy")
         {
-            //プレイヤーの攻撃力とスキルのダメージ
-            Player player = GameObject.Find("Player").GetComponent<Player>();
-            enemy.HitDamage(player._attack * _skillDamage);
+            //プレイヤーの攻撃力とスキルのダメージで計算して敵にダメージを与える
+            enemy.HitDamage(_player._attack * _skillDamage);
         }
     }
 }
