70ddf6ea1a0927b583e6c63caa62f1f6ca0ac8b0
sekineseishu <mika242401@gmail.com>
2024-08-21 21:46:26 +0900

    魔法陣スキルの生成やレベルアップ時の処理を管理

--- Assets/Script/MagicManager.cs
+++ Assets/Script/MagicManager.cs
@@ -5,17 +5,18 @@ using TMPro;
 
 public class MagicManager : MonoBehaviour
 {
-    [SerializeField] Skill _skill;
+    [SerializeField] Skill _skill;//スキルデータ
     [SerializeField] private Magic MagicPrehub;//スキルのプレハブ
     [SerializeField] private Player _player;
     [SerializeField] private float _coolTime;
-    private bool _spawnTrigger;
-    private bool _nowSkill;
+    private bool _spawnTrigger;//スキルを生成できるかフラグ
+    private bool _nowSkill;//フィールドにスキルがあるかのフラグ
 
 
     // Start is called before the first frame update
     void Start()
     {
+        //スキルデータを渡して初期化
         _spawnTrigger = false;
         _coolTime = _skill._coolTime;
     }
@@ -25,19 +26,21 @@ public class MagicManager : MonoBehaviour
     {
         if (_spawnTrigger)
         {
-            if (!_nowSkill)
+            if (!_nowSkill)//フィールドにスキルがなければ実行
             {
                 _coolTime -= Time.deltaTime;
                 if (_coolTime <= 0)
                 {
                     transform.position = _player.transform.position;
                     Magic magic = Instantiate(MagicPrehub, gameObject.transform);
+                    //スキルにプレイヤーの情報を与える(ダメージを与える際にプレイヤーの攻撃力を使うため)
                     magic._player = _player;
                     _nowSkill = true;
                 }
             }
             else
             {
+                //全てのスキルが消えたらクールタイムをリセットする
                 if (transform.childCount == 0)
                 {
                     Respawn();
@@ -48,33 +51,36 @@ public class MagicManager : MonoBehaviour
 
     private void Respawn()
     {
+        //リセット
         _nowSkill = false;
         _coolTime = _skill._coolTime;
     }
 
+    //スキルレベルが上がった際に呼び出す
     public void SkillLevelBounas()
     {
-        if (_skill._skillLevel == 1)
+        //スキルのレベルが上がることに効果をプラスする
+        if (_skill._skillLevel == 1)//スキルを発動可能にする
         {
             _spawnTrigger = true;
         }
-        if (_skill._skillLevel == 2)
+        if (_skill._skillLevel == 2)//スキルのダメージを上げる
         {
             _skill._damage = 40;
         }
-        if (_skill._skillLevel == 3)
+        if (_skill._skillLevel == 3)//スキルのダメージを上げる
         {
             _skill._damage = 50;
         }
-        if (_skill._skillLevel == 4)
+        if (_skill._skillLevel == 4)//スキルのダメージを上げる
         {
             _skill._damage = 60;
         }
-        if (_skill._skillLevel == 5)
+        if (_skill._skillLevel == 5)//スキルのダメージを上げる
         {
             _skill._damage = 70;
         }
-        if (_skill._skillLevel == 6)
+        if (_skill._skillLevel == 6)//スキルのクールタイムを短縮する
         {
             _skill._coolTime = 10;
         }
